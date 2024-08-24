b936da610984fc7323cf2d47c970687942f93f86
hoanghai912 <haih2212@gmail.com>
2024-08-24 12:34:21 +0700

    Add ESLint for check syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
@@ -15,99 +15,99 @@ app.use(cors())
 app.use(express.static('dist'))
 
 // Routes
-app.get('/',(req,res)=>{
-    res.send('<h1>Welcome to Todo-list backend. Visit <a href="/api/todos">/api/todos</a> for more info</h1>')
+app.get('/',(req,res) => {
+  res.send('<h1>Welcome to Todo-list backend. Visit <a href="/api/todos">/api/todos</a> for more info</h1>')
 })
 
-app.get('/api/todos',(req,res,next)=>{
-    Todos.find({})
-        .then(todos => res.json(todos))
-        .catch(error => next(error))
+app.get('/api/todos',(req,res,next) => {
+  Todos.find({})
+    .then(todos => res.json(todos))
+    .catch(error => next(error))
 })
 
-app.get('/api/todos/:id',(req,res,next)=>{
-    const id = req.params.id
-    Todos.findById(id)
-        .then(todo => {
-            if (todo) res.json(todo)
-            else res.status(404).json({error:'Not found'})
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/todos/:id',(req,res,next)=>{
-    const id = req.params.id
-    Todos.findByIdAndDelete(id)
-        .then(todo => {
-            if (todo) res.status(204).end()
-            else res.status(404).json({error:'Not found'})
-        })
-        .catch(error => next(error))
-})
-
-app.put('/api/todos/:id',(req,res,next)=>{
-    const id = req.params.id
-    const body = req.body
-    // const checkKeys = [ 'title', 'complete', 'time', 'date', 'category' ]
-    // if (!checkKeys.every(key => key in body)) {
-    //     return res.status(400).json({error: 'Try again. Data is missing.'})
-    // }
-
-    const newTodo = {
-        title: body.title, 
-        complete: Boolean(body.complete),
-        time: body.time,
-        date: body.date,
-        category: body.category
-    }
-
-    Todos.findByIdAndUpdate(id, newTodo, {new: true})
-        .then(updateTodo => {
-            if (updateTodo) res.json(updateTodo)
-            else res.status(404).json({error:'Not found'})
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/todos',(req,res,next)=>{
-    const body = req.body
-    // const checkKeys = [ 'title', 'complete', 'time', 'date', 'category' ]
-    // if (!checkKeys.every(key => key in body)) {
-    //     return res.status(400).json({error: 'Try again. Data is missing.'})
-    // }
-
-    const newTodo = new Todos({
-        title: body.title, 
-        complete: Boolean(body.complete),
-        time: body.time,
-        date: body.date,
-        category: body.category
+app.get('/api/todos/:id',(req,res,next) => {
+  const id = req.params.id
+  Todos.findById(id)
+    .then(todo => {
+      if (todo) res.json(todo)
+      else res.status(404).json({ error:'Not found' })
     })
+    .catch(error => next(error))
+})
 
-    newTodo.save()
-        .then(todo => res.json(todo))
-        .catch(error => next(error))
+app.delete('/api/todos/:id',(req,res,next) => {
+  const id = req.params.id
+  Todos.findByIdAndDelete(id)
+    .then(todo => {
+      if (todo) res.status(204).end()
+      else res.status(404).json({ error:'Not found' })
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/todos/:id',(req,res,next) => {
+  const id = req.params.id
+  const body = req.body
+  // const checkKeys = [ 'title', 'complete', 'time', 'date', 'category' ]
+  // if (!checkKeys.every(key => key in body)) {
+  //     return res.status(400).json({error: 'Try again. Data is missing.'})
+  // }
+
+  const newTodo = {
+    title: body.title,
+    complete: Boolean(body.complete),
+    time: body.time,
+    date: body.date,
+    category: body.category
+  }
+
+  Todos.findByIdAndUpdate(id, newTodo, { new: true })
+    .then(updateTodo => {
+      if (updateTodo) res.json(updateTodo)
+      else res.status(404).json({ error:'Not found' })
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/todos',(req,res,next) => {
+  const body = req.body
+  // const checkKeys = [ 'title', 'complete', 'time', 'date', 'category' ]
+  // if (!checkKeys.every(key => key in body)) {
+  //     return res.status(400).json({error: 'Try again. Data is missing.'})
+  // }
+
+  const newTodo = new Todos({
+    title: body.title,
+    complete: Boolean(body.complete),
+    time: body.time,
+    date: body.date,
+    category: body.category
+  })
+
+  newTodo.save()
+    .then(todo => res.json(todo))
+    .catch(error => next(error))
 })
 
 // Unknown route
 const unknownEndpoint = (req, res) => {
-    return res.status(404).send({ error: 'unknown endpoint' })
+  return res.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    }
-    if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 const PORT= process.env.PORT
-app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`))
+app.listen(PORT,() => console.log(`Server is running on PORT ${PORT}`))
--- models/todo.js
+++ models/todo.js
@@ -3,59 +3,59 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(() => console.log('connected to MongoDB'))
-    .catch((error) => console.log('error connecting to MongoDB:', error.message))
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
 
 mongoose.set('strictQuery', false)
 
 const todoSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  complete: {
+    type: Boolean,
+    required: true,
+  },
+  time: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2}:\d{2}/.test(v)
+      },
+      message: props => `${props.value} is not a valid time`
     },
-    complete: {
-        type: Boolean,
-        required: true,
+    required: true,
+  },
+  date: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{4}-\d{2}-\d{2}/.test(v)
+      },
+      message: props => `${props.value} is not a valid date`
     },
-    time: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return /\d{2}:\d{2}/.test(v)
-            },
-            message: props => `${props.value} is not a valid time`
-        },
-        required: true,
+    require: true,
+  },
+  category: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        console.log(v)
+        return ['work', 'personal', 'freelance'].includes(v.toLowerCase())
+      },
+      message: props => `${props.value} is not a valid category`
     },
-    date: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return /\d{4}-\d{2}-\d{2}/.test(v)
-            },
-            message: props => `${props.value} is not a valid date`
-        },
-        require: true,
-    },
-    category: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                console.log(v)
-                return ['work', 'personal', 'freelance'].includes(v.toLowerCase())
-            },
-            message: props => `${props.value} is not a valid category`
-        },
-        required: true
-    },
+    required: true
+  },
 })
 
 todoSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Todos', todoSchema)
